e4f1dfe3c395a1865c2b4081143a569850c4d7b8
Abdullah Arif <arif115@uwindsor.ca>
2021-01-24 11:41:35 -0500

    made satisfaction dependent on agent model radius,

--- Assets/Scripts/GameBrains/Minds/SeekerMind.cs
+++ Assets/Scripts/GameBrains/Minds/SeekerMind.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using GameBrains.Actions;
 using GameBrains.Entities.Agents;
 using GameBrains.Percepts;
 using UnityEngine;
 
+/* Mind that figures out where and how to move to a target block */
 namespace GameBrains.Minds
 {
     public class SeekerMind : Mind
@@ -11,7 +12,7 @@ namespace GameBrains.Minds
         // TODO: Make parameters settable through Mind decisions??
         
         // How close is close enough?
-        [SerializeField] protected float desiredSatisfactionRadius = 0.5f; // TODO: Should depend on Agent radius??
+        [SerializeField] protected float desiredSatisfactionRadius = 0f; // TODO: Should depend on Agent radius??
         // How fast should we move?
         [SerializeField] protected float desiredSpeed = 100f; // TODO: Should depend on Actuator capabilities??
         [SerializeField] protected float moveTimeToLive = 5f;
@@ -19,9 +20,44 @@ namespace GameBrains.Minds
         [SerializeField] protected float desiredSatisfactionAngle = 2; // TODO: Should depend on Actuator capabilities??
         [SerializeField] protected float desiredAngularSpeed = 1f; // TODO: Should depend on Actuator capabilities??
         [SerializeField] protected float turnTimeToLive = 5f;
+        
+        /* We want to get the scale of the object relative to the world - but unity only gives
+        * the local scale:
+        * solution from: https://forum.unity.com/threads/world-scale.2270/
+        */
+        protected Vector3 GetWorldScale(Transform transform)
+            {
+                Vector3 worldScale = transform.localScale;
+                Transform parent = transform.parent;
+               
+                while (parent != null)
+                {
+                    worldScale = Vector3.Scale(worldScale,parent.localScale);
+                    parent = parent.parent;
+                }
+               
+                return worldScale;
+            }
+        protected override void Awake(){
+            base.Awake();
+            /* NOTE: we are using tags to avoid manually setting up the script
+            *   However, this makes it so the objects must be tagged correctly 
+            */
+            var agentObject = GameObject.FindWithTag("Player");
+            var agentMesh = agentObject.GetComponent<MeshRenderer>();
+            var agentObjectTransform = GetWorldScale(agentObject.GetComponent<Transform>());
+            Bounds bounds = agentMesh.bounds;
+            /*NOTE: we use the minimum of the two bounds because we don't want to constraint 
+            * the shape of the agent 
+            * we only look at the bounds for x and z because our agent only moves along the x
+            *   and z axis. It might make sense to have a separate radius for the x and z
+            * instead of combining them...
+            */
+            desiredSatisfactionRadius = Mathf.Max(bounds.extents.x * agentObjectTransform.x ,
+                                                    bounds.extents.z * agentObjectTransform.z);
+        }
 
-
-
+        /* Bot chooses target depending on mode and then goes to the target */
         public override List<Action> Think(IEnumerable<Percept> percepts)
         {
             Vector3 targetPosition;
@@ -29,7 +65,7 @@ namespace GameBrains.Minds
             Transform agentTransform = Agent.transform;
             Vector3 agentPosition = agentTransform.position;
             targetPosition = ChooseTargetPosition(percepts);
-
+            
             // TODO: If Actuator cannot achieve desire this keeps trying endlessly??
             if (Vector3.Distance(agentPosition, targetPosition) > desiredSatisfactionRadius)
             {
